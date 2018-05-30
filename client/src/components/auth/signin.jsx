845feaa596519c6e1d95bb9f968e6f3d20f6f585
gehrig rankin <rgrankin22@gmail.com>
2018-05-30 14:24:14 -0700

    styled the signin form

--- client/src/components/auth/signin.jsx
+++ client/src/components/auth/signin.jsx
@@ -22,23 +22,65 @@ class Signin extends Component {
     const { handleSubmit, fields: { email, password }} = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <div className="field">
-          <p class="control has-icons-left ">
-            <input {...email} className="input" type="email" placeholder="Email"/>
+      // <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+      //   <div className="field">
+      //     <p className="control has-icons-left ">
+      //       <input {...email} className="input" type="email" placeholder="Email"/>
           
-            <span class="icon is-small is-left">
-              <i class="fas fa-envelope"></i>
-            </span>
-          </p>
+      //       <span className="icon is-small is-left">
+      //         <i className="fas fa-envelope"></i>
+      //       </span>
+      //     </p>
+      //   </div>
+      //   <div className="field">
+      //     <label>Password:</label>
+      //     <input {...password} type="password" className="form-control" />
+      //   </div>
+      //   {this.renderAlert()}
+      //   <button action="submit" className="btn btn-primary">Sign in</button>
+      // </form>
+
+      <div id="signin-form" className="column has-background-white">
+        <div className="tabs is-centered">
+          <ul>
+            <li className="is-active">
+              <a>Sign In</a>
+            </li>
+            <li>
+              <a>
+                <span>Sign Up</span>
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="field">
-          <label>Password:</label>
-          <input {...password} type="password" className="form-control" />
-        </div>
-        {this.renderAlert()}
-        <button action="submit" className="btn btn-primary">Sign in</button>
-      </form>
+        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+          <div className="field">
+            <label className="label">Email:</label>
+            
+            <div className="control has-icons-left">
+              <input {...email} type="text" className="input" placeholder="Email"/>
+
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+             </span>
+            </div>         
+          </div>
+          <div className="field">
+            <label className="label">Password:</label>
+            
+            <div className="control has-icons-left">
+              <input {...password} type="text" className="input" placeholder="******"/>
+
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+             </span>
+            </div>         
+          </div>
+          
+          <a action="submit" className="button has-background-danger has-text-white">Sign in</a>
+          
+        </form>
+      </div>
     );
   }
 }
